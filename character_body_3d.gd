ada91ca1fdcfa1ef41e7acf3f257fbd645a56f3e
LeDogetier <145688505+LeDogetier@users.noreply.github.com>
2024-01-05 17:35:51 -0500

    camera fix ish

--- character_body_3d.gd
+++ character_body_3d.gd
@@ -1,5 +1,6 @@
 extends CharacterBody3D
 
+const SPEED: int = 7
 
 func _ready():
 	pass
@@ -9,14 +10,18 @@ func _physics_process(delta):
 	if Input.is_action_pressed("ui_left") and Input.is_action_pressed("ui_right"):
 		velocity.x = 0
 	elif Input.is_action_pressed("ui_left"):
-		velocity.x = 5
+		velocity.x = SPEED
 	elif Input.is_action_pressed("ui_right"):
-		velocity.x = -5
+		velocity.x = -SPEED
+	else:
+		velocity.x = lerpf(velocity.x,0,0.1)
 	
 	if Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_down"):
 		velocity.z = 0
 	elif Input.is_action_pressed("ui_up"):
-		velocity.z = 5
+		velocity.z = SPEED
 	elif Input.is_action_pressed("ui_down"):
-		velocity.z = -5
+		velocity.z = -SPEED
+	else:
+		velocity.z = lerpf(velocity.z,0,0.1)
 	move_and_slide()
